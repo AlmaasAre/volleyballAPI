f2a11c4c5744265e8d1f4b8f62c3b875f92a79b1
Are Almaas <mralmaas@gmail.com>
2013-10-25 09:58:02 +0200

    removed limit on getAll

--- app.js
+++ app.js
@@ -25,13 +25,8 @@ app.get('/', function(req, res) {
     res.send('please select a collection, e.g., /collections/messages')
 });
 
-app.get('/monkey', function (req, res) {
-    res.send({name:'HOHOHO'});
-});
-
-
 app.get('/collections/:collectionName', function(req, res) {
-    req.collection.find({},{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+    req.collection.find({},{sort: [['_id',-1]]}).toArray(function(e, results){
         if (e) return next(e)
         res.send(results)
     })
